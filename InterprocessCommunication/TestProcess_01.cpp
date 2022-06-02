36d9a9263281c2eb64edcc5d4d322bd0d993effd
chenshougang <1396748781@qq.com>
2022-06-02 14:26:03 +0800

    add interprocess communication

---
+++ InterprocessCommunication/TestProcess_01.cpp
@@ -0,0 +1,32 @@
+﻿// ServerProcess.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+
+#define INPUT_SIZE  4096   //内存空间的大小
+#define PROCESS_NAME  L"MemoryShare"  //访问地址的标志，类似于字典中的key
+#define  _CRT_SECURE_NO_WARNINGS 
+
+using namespace std;
+
+int main()
+{
+	char input_str[] = "test data";//存放的数据
+	//1.创建共享内存
+	HANDLE hFileMap_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_EXECUTE_READWRITE, 0, INPUT_SIZE, PROCESS_NAME);
+	//2.得到共享内存的指针
+	LPVOID share_memory_ptr = MapViewOfFile(hFileMap_Handle, FILE_MAP_ALL_ACCESS, 0, 0, INPUT_SIZE);
+	//3.把需要客户端读取的数据放在共享内存的地址上
+
+	strcpy((char*)share_memory_ptr, input_str);
+
+	Sleep(5000000);//设置等待时间，即为内存的存放时间，等待完就释放
+
+	//4.解除映射
+	UnmapViewOfFile(share_memory_ptr);
+	//5.关闭句柄
+	CloseHandle(hFileMap_Handle);
+
+	cout << "访问结束" << endl;
+}
---
+++ InterprocessCommunication/TestProcess_02.cpp
@@ -0,0 +1,32 @@
+﻿// ClientProcess.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include <Windows.h>
+#define INPUT_SIZE  4096   //内存地址的大小
+#define PROCESS_NAME  L"MemoryShare"
+#define  _CRT_SECURE_NO_WARNINGS 
+
+using namespace std;
+
+int main()
+{
+	//打开共享的文件对象
+	HANDLE hFileMap_Handle = OpenFileMapping(FILE_MAP_ALL_ACCESS, NULL, PROCESS_NAME);
+	//判断句柄是否存在
+	if (hFileMap_Handle)
+	{
+		LPVOID lpBase = MapViewOfFile(hFileMap_Handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+		char szBuffer[INPUT_SIZE] = { 0 };
+		strcpy(szBuffer, (char*)lpBase);
+
+		while (1) {
+			cout << "读取的数据为：" << szBuffer << endl;
+		}
+
+		//解除映射
+		UnmapViewOfFile(lpBase);
+		//关闭句柄
+		CloseHandle(hFileMap_Handle);
+	}
+
+}
